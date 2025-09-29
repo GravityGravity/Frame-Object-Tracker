250cf4a43627c70eb05f10ddd0b268de84f32ee1
GravityGravity <154651849+GravityGravity@users.noreply.github.com>
2025-09-29 13:15:57 -0400

    Added for loop that iterates 29 times so it can capture 30 frames
    Added two new vars one for comparison of a processed
    image versus an unprocessed image

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Project 2: Video-Object-Tracker
+
 import sys
 import cv2 as cv
 import numpy as py
@@ -8,19 +10,23 @@ import numpy as py
 
 # process all 30 frame videos in 10 seconds
 
+#Example Input: $ python project2.py /path/to/video/
 
 def main():
 
     # Debug: Check image path was passed
     try:
         print(f'Image path passed: {sys.argv[1]}')
+        videoPath = sys.argv[1]
     except IndexError:
         sys.exit('ERROR: No Argument for image path')
 
     # Read image and Check image was read
-    orgImg = cv.imread(sys.argv[1], cv.IMREAD_COLOR_BGR)
+    for i in range(29):
+        curImg = cv.imread(videoPath, cv.IMREAD_GRAYSCALE) #Current image that was processed
+        nxtImg = cv.imread(videoPath, cv.IMREAD_GRAYSCALE) #Next image being processed and compared to first
 
-    if orgImg is None:
-        sys.exit('ERROR: Could not read image')
+    if curImg or nxtImg is None:
+        sys.exit('ERROR: Could not read image or video read ended')
 
     return 0
