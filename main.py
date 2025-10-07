00eb01b63dd4f5611afbef3caa83e985a6db4ed5
Gravity <isaakhern@outlook.com>
2025-10-07 13:17:07 -0400

    Frame Pre-Processing
    Added Frame print out iterated 30 times
        On key press 'Q' or 'q' the loop will exit completely
        On any other key press COntinue loop frame preview
    
    (Not Functioning) Attempted to add initial rectangle printout
        Print out loop Iterative and rectangle coordinates

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import numpy as py
 
 # process all 30 frame videos in 10 seconds
 
-#Example Input: $ python project2.py /path/to/video/
+# Example Input: $ python project2.py /path/to/video/
+
 
 def main():
 
@@ -18,15 +19,59 @@ def main():
     try:
         print(f'Image path passed: {sys.argv[1]}')
         videoPath = sys.argv[1]
+
+        with open(videoPath + '\\rect.txt', "r", encoding='utf-8') as file:
+            initRect = file.read()
+            initRect = initRect.split()
+            initRect = list(map(int, initRect))
+
+            Xpos, Ypos, Width, Height = initRect
+
     except IndexError:
         sys.exit('ERROR: No Argument for image path')
 
+    # DEBUG:  Print videoPath and Initial Rectangle
+    print(f'videoPath: {videoPath}')
+    print(f'initial rectangle: {initRect}')
+
+    quitKey = cv.waitKey(1) & 0xFF
+
     # Read image and Check image was read
     for i in range(29):
-        curImg = cv.imread(videoPath, cv.IMREAD_GRAYSCALE) #Current image that was processed
-        nxtImg = cv.imread(videoPath, cv.IMREAD_GRAYSCALE) #Next image being processed and compared to first
+        curPathItr = videoPath + f'\\frame{i}.png'
+        nxtPathItr = videoPath + f'\\frame{i+1}.png'
 
-    if curImg or nxtImg is None:
-        sys.exit('ERROR: Could not read image or video read ended')
+        # DEBUG: Print i itr for loop
+        print(f'({i})   curPath: {curPathItr}')
+
+        if quitKey == ord('q'):
+            break
+
+        # Current image that was processed
+        curImg = cv.imread(curPathItr, cv.IMREAD_COLOR_BGR)
+        # Next image being processed and compared to first
+        nxtImg = cv.imread(nxtPathItr, cv.IMREAD_COLOR_BGR)
+
+        if curImg is None or nxtImg is None:
+            sys.exit('ERROR: Could not read image or video read ended')
+
+        Width = Xpos - Width
+        Height = Ypos - Height
+
+        cv.rectangle(curImg, (Xpos, Ypos),
+                     (Width, Height), (255, 0, 0), 3)
+
+        print(f'{Xpos} {Ypos} {Width} {Height}')
+
+        cv.imshow(f'frame{i}', curImg)
+
+        quitKey = cv.waitKey(0) & 0xFF
+        if quitKey == ord('q') or quitKey == ord('Q'):
+            break
+
+    print()
 
     return 0
+
+
+main()
